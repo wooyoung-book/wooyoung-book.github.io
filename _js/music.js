4e5a15397a9f105ce72a7c558abeba2b1cc24e3e
wooyoung-book <gcwy@mailbox.org>
2024-10-13 13:07:52 +0000

    Delete _js directory

--- _js/music.js
+++
@@ -1,7 +0,0 @@
-fetch('../_html/music.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('music').innerHTML = data;
-    })
-    .catch(error => console.error('Error fetching HTML:', error));
-
--- _js/review.js
+++
@@ -1,6 +0,0 @@
-fetch('_html/review.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('review').innerHTML = data;
-    })
-    .catch(error => console.error('Error fetching HTML:', error));
