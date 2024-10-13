bc633cac513bc97f46b978fe63ed3195332fe4b7
wooyoung-book <gcwy@mailbox.org>
2024-10-13 12:19:17 +0000

    Update music.js

--- _js/music.js
+++ _js/music.js
@@ -1,6 +1,11 @@
+
+function loadHTML(file) {
 fetch('_html/music.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('music').innerHTML = data;
     })
     .catch(error => console.error('Error fetching HTML:', error));
+}
+
+loadHTML('_html/music.html');
