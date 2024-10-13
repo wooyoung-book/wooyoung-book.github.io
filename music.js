c3715127d41dd225e0b9e8457a43d73cd111bdc4
wooyoung-book <gcwy@mailbox.org>
2024-10-13 07:34:28 +0000

    music.js 업데이트

--- music.js
+++ music.js
@@ -1,6 +1,6 @@
-fetch('music.md')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('music').innerText = data;
-})
-  .catch(error => console.error('Error fetching file:', error));
+fetch('music.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('music').innerHTML = data;
+    })
+    .catch(error => console.error('Error fetching HTML:', error));
