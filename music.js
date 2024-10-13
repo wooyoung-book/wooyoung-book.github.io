ff9d0ff7e28c87aab0e83557cf23e6f906a1c0b5
wooyoung-book <gcwy@mailbox.org>
2024-10-13 11:22:49 +0000

    Delete music.js

--- music.js
+++
@@ -1,6 +0,0 @@
-fetch('music.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('music').innerHTML = data;
-    })
-    .catch(error => console.error('Error fetching HTML:', error));
