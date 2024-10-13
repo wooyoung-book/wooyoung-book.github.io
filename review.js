634c9d3c68a88112136806cda5fc8f94e7ce1bd0
wooyoung-book <gcwy@mailbox.org>
2024-10-13 12:50:52 +0000

    Create review.js

---
+++ review.js
@@ -0,0 +1,6 @@
+fetch('review.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('review').innerHTML = data;
+    })
+    .catch(error => console.error('Error fetching HTML:', error));
