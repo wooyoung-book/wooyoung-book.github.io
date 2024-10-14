9f549057f164db87d3b98bb4db429b22bc0a7cac
wooyoung-book <gcwy@mailbox.org>
2024-10-14 15:48:38 +0000

    Delete video.js

--- video.js
+++
@@ -1,6 +0,0 @@
-fetch('video.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('video').innerHTML = data;
-    })
-    .catch(error => console.error('Error fetching HTML:', error));
