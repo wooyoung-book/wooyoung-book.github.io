31719f14fc7e40b24fce217afb195a3080941e19
wooyoung-book <gcwy@mailbox.org>
2024-10-22 17:57:15 +0000

    Update music.js

--- music.js
+++ music.js
@@ -89,15 +89,15 @@ document.addEventListener("DOMContentLoaded", function() {
                    data-video-id="${link.id}" 
                    style="
                        display: block; 
-                       padding: 0; 
-                       background-color: #fff; 
-                       color: #000; 
-                       text-decoration: none; 
-                       border-bottom: 1px solid #000; 
-                       transition: background-color 0.3s, transform 0.2s; 
-                       font-size: 14px; 
-                       cursor: pointer;" 
-                   onmouseover="this.style.backgroundColor='#98FF98'; this.style.transform='scale(1.1) translateX(10px)';" 
+                       padding: 0;
+                       background-color: #fff;
+                       color: #000;
+                       text-decoration: none;
+                       border-bottom: 1px solid #000;
+                       transition: background-color 0.3s, transform 0.2s;
+                       font-size: 14px;
+                       cursor: pointer;"
+                   onmouseover="this.style.backgroundColor='#98FF98'; this.style.transform='scale(1.1) translateX(10px)';"
                    onmouseout="this.style.backgroundColor='#fff'; this.style.transform='scale(1) translateX(0)';">
                     ${link.label}
                 </a>
