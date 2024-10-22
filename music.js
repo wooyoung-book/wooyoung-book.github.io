3cd9f03f712216e3879ed8ea18834fc15f6ffc7c
wooyoung-book <gcwy@mailbox.org>
2024-10-22 15:21:09 +0000

    Update music.js

--- music.js
+++ music.js
@@ -80,8 +80,7 @@ document.addEventListener("DOMContentLoaded", function() {
                        border-radius: 4px; 
                        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
                        transition: background-color 0.3s, transform 0.2s;
-                       font-size: 14px; 
-                   "
+                       font-size: 14px;"
                    onmouseover="this.style.backgroundColor='#9e9e9e'; this.style.transform='scale(1.05)';"
                    onmouseout="this.style.backgroundColor='#e0e0e0'; this.style.transform='scale(1)';">
                     ${link.label}
