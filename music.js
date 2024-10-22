214f1536bfee02919c5b2409f753d7fb8c4b3e5d
wooyoung-book <gcwy@mailbox.org>
2024-10-22 17:38:31 +0000

    Update music.js

--- music.js
+++ music.js
@@ -97,8 +97,8 @@ document.addEventListener("DOMContentLoaded", function() {
                        transition: background-color 0.3s, transform 0.2s; 
                        font-size: 14px; 
                        cursor: pointer;" 
-                   onmouseover="this.style.backgroundColor='#98FF98'; this.style.transform='scale(1.1) translateX(10px)';" 
-                   onmouseout="this.style.backgroundColor='#fff'; this.style.transform='scale(1) translateX(0)';">
+                   onmouseover="this.style.backgroundColor='#98FF98'; this.style.transform='scale(1.1)';" 
+                   onmouseout="this.style.backgroundColor='#fff'; this.style.transform='scale(1)';">
                     ${link.label}
                 </a>
             </div>
