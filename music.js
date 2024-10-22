895be72a94b0a0958e89112568979c98a725bc37
wooyoung-book <gcwy@mailbox.org>
2024-10-22 16:04:51 +0000

    Update music.js

--- music.js
+++ music.js
@@ -53,18 +53,18 @@ document.addEventListener("DOMContentLoaded", function() {
     function createLinksHTML() {
         const links = [
             { id: "pkbXucb7mtA", label: "▶️ Bola - Para Qweqway" },
-            { id: "d_34u3yowvE", label: "Sounds From The Ground - This Land" },
-            { id: "RgmufUgVmi8", label: "Shadowy Men On A Shadowy Planet - Zombie Compromise" },
-            { id: "mmCnQDUSO4I", label: "Dmitri Shostakovich - Waltz No. 2" },
-            { id: "0VpQi7EOEDg", label: "Drakphaser - Phasius Earth" },
-            { id: "oAN_UVHtCro", label: "Dalot - Infinite Window" },
-            { id: "hxdfiHGrcCA", label: "winterlight - Between Joy" },
-            { id: "ZCDAszFV-7U", label: "Damjan Mravunac - False God" },
-            { id: "QMV3A65PTG0", label: "S1gns of L1fe - Synesthetic State" },
-            { id: "HhmHj1Wn5s4", label: "Dav Dralleon - Sword Ov Saturn" },
-            { id: "Q13-FiOJvFk", label: "Quench - Slick" },
-            { id: "Jydilwi-ric", label: "Rechenzentrum - Happy End" },
-            { id: "w9sSkEWbopA", label: "Sense - Walking Water" },
+            { id: "d_34u3yowvE", label: "▶️ Sounds From The Ground - This Land" },
+            { id: "RgmufUgVmi8", label: "▶️ Shadowy Men On A Shadowy Planet - Zombie Compromise" },
+            { id: "mmCnQDUSO4I", label: "▶️ Dmitri Shostakovich - Waltz No. 2" },
+            { id: "0VpQi7EOEDg", label: "▶️ Drakphaser - Phasius Earth" },
+            { id: "oAN_UVHtCro", label: "▶️ Dalot - Infinite Window" },
+            { id: "hxdfiHGrcCA", label: "▶️ winterlight - Between Joy" },
+            { id: "ZCDAszFV-7U", label: "▶️ Damjan Mravunac - False God" },
+            { id: "QMV3A65PTG0", label: "▶️ S1gns of L1fe - Synesthetic State" },
+            { id: "HhmHj1Wn5s4", label: "▶️ Dav Dralleon - Sword Ov Saturn" },
+            { id: "Q13-FiOJvFk", label: "▶️ Quench - Slick" },
+            { id: "Jydilwi-ric", label: "▶️ Rechenzentrum - Happy End" },
+            { id: "w9sSkEWbopA", label: "▶️ Sense - Walking Water" },
         ];
 
         return links.map(link => `
@@ -73,7 +73,7 @@ document.addEventListener("DOMContentLoaded", function() {
                    data-video-id="${link.id}" 
                    style="
                        display: block; 
-                       padding: 0 30px; 
+                       padding: 0 10px; 
                        background-color: #fff;
                        color: #000;
                        text-decoration: none; 
