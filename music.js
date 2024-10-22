79897d00a4f1356d94156ec65292a2e9b337b0db
wooyoung-book <gcwy@mailbox.org>
2024-10-22 15:42:50 +0000

    Update music.js

--- music.js
+++ music.js
@@ -75,7 +75,7 @@ document.addEventListener("DOMContentLoaded", function() {
                        display: block; 
                        padding: 0 5px; 
                        background-color: #fff; 
-                       color: #fff; 
+                       color: #000; 
                        text-decoration: none; 
                        border-top: 1px solid #000; 
                        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
