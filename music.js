5dfa82859c8738ff8387406b0fb8dba2cf11ed99
wooyoung-book <gcwy@mailbox.org>
2024-10-23 14:54:44 +0000

    Update music.js

--- music.js
+++ music.js
@@ -165,7 +165,7 @@ window.onpageshow = function(event) {
     if ( event.persisted || (window.performance && window.performance.navigation.type == 2)) {
         // Back Forward Cache로 브라우저가 로딩될 경우 혹은 브라우저 뒤로가기 했을 경우
         // 이벤트 추가하는 곳
-                   const result = confirm(currvideoId);
+                   const result = confirm("ㄴㅇㄹㄴㅇㄹ");
                                 if (result) {
                                     console.log('사용자가 확인했습니다.');
                                 } else {
